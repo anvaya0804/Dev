57f61c2f30ad4943456fd4783230858be82bba58
anvaya0804 <82364698+anvaya0804@users.noreply.github.com>
2021-04-14 18:28:40 +0530

    Add files via upload

---
+++ src/main/java/com/omnireach/SpringMVCHibernateCRUDExample/SpringMvcHibernateCrudExampleApplication.java
@@ -0,0 +1,13 @@
+package com.omnireach.SpringMVCHibernateCRUDExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcHibernateCrudExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcHibernateCrudExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omnireach/SpringMVCHibernateCRUDExample/SpringMvcHibernateCrudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omnireach.SpringMVCHibernateCRUDExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcHibernateCrudExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
